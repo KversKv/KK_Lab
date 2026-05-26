22e730c4da363777a8cb89f2ae97d8d1837076d2
kversk <kversk@qq.com>
2026-05-26 11:30:23 +0800

    Fixed datalog open main

--- ui/pages/n6705c_power_analyzer/n6705c_datalog_ui.py
+++ ui/pages/n6705c_power_analyzer/n6705c_datalog_ui.py
@@ -7,6 +7,14 @@
 
 import sys
 import os
+
+if __name__ == "__main__" and __package__ in (None, ""):
+    _PROJECT_ROOT = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
+    )
+    if _PROJECT_ROOT not in sys.path:
+        sys.path.insert(0, _PROJECT_ROOT)
+
 from ui.resource_path import get_resource_base
 import math
 import random
